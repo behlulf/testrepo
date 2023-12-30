06c30fe4f4cbdda36c00be84b840f7cad48f91c3
behlulf <155235539+behlulf@users.noreply.github.com>
2023-12-30 16:19:07 +0300

    Add files via upload

---
+++ DataScienceToolsandEcosystem.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In[2]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# List popular languages for Data Science
+# - python
+# - R
+# - Julia
+# - SQL
+
+# ## Author: Behlul Filizlibay
+
+# In[ ]:
+
+
+
+
